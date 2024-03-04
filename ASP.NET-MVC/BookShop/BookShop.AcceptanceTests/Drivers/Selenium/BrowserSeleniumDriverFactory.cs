b66b4134376653cb03dfd94a830d99fb0b7474a3
Jonas Thyregod <JONTH@DSB.DK>
2024-03-04 10:27:06 +0100

    Updated to .NET 8.0 (also nuget packages)

--- ASP.NET-MVC/BookShop/BookShop.AcceptanceTests/Drivers/Selenium/BrowserSeleniumDriverFactory.cs
+++ ASP.NET-MVC/BookShop/BookShop.AcceptanceTests/Drivers/Selenium/BrowserSeleniumDriverFactory.cs
@@ -6,6 +6,7 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Edge;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.IE;
+// ReSharper disable AssignNullToNotNullAttribute
 
 namespace BookShop.AcceptanceTests.Drivers.Selenium
 {
